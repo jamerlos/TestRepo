5ae29ff95b2534ec032daf82a240da4f5346957d
John Merlos <jm@jm.com>
2017-05-20 16:17:19 -0700

    Commiting

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -28,6 +28,7 @@ public class JavaApplication1 {
         System.err.println( "Goodbye" );
         System.err.println( "Hello from the other sidef" );
         System.err.println( "Hello from the other sidef" );
+        System.err.println( "Hello from the other sidef" );
     }
     
 }
